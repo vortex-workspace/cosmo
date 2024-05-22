4ae35adf3985f5720267dba242d8d131ffc87e93
mex3890 <abcv.tenorio@gmail.com>
2024-05-21 23:04:54 -0300

    Install Psy package

--- src/Commands/CosmoShell.php
+++ src/Commands/CosmoShell.php
@@ -4,12 +4,23 @@ namespace Cosmo\Commands;
 
 use Cosmo\Command;
 use Cosmo\Command\Enums\CommandResponse;
+use Psy\Configuration;
+use Psy\Shell;
 
 class CosmoShell extends Command
 {
     protected function handle(): CommandResponse
     {
+        $config = new Configuration([
+            'colorMode' => Configuration::COLOR_MODE_AUTO,
+            'errorLoggingLevel' => E_ALL,
+            'forceArrayIndexes' => true,
+            'historySize' => 10,
+            'prompt' => '>>>',
+        ]);
 
+        $shell = new Shell($config);
+        $shell->run();
         return CommandResponse::SUCCESS;
     }
 
